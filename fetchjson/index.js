5c1293135eaf2f995392b07166aac8285aea960c
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2023-05-08 20:11:50 +0530

    first app, interface, adding type annotations

---
+++ fetchjson/index.js
@@ -0,0 +1,16 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var axios_1 = require('axios');
+var URL = 'https://jsonplaceholder.typicode.com/todos/1';
+axios_1.default.get(URL).then(function (response) {
+  var todo = response.data;
+  var id = todo.id;
+  var title = todo.title;
+  var completed = todo.completed;
+  console.log(
+    '\n  The Todo with ID: '
+      .concat(id, '\n  Has a title of: ')
+      .concat(title, '\n  Is it finished? ')
+      .concat(completed, '\n  ')
+  );
+});
